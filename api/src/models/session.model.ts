2f02040acd844accccb8936dcdcb16245d212d72
Mark_Leo <ratrateroo@gmail.com>
2022-10-30 23:09:46 +0800

    fixed schema interface

--- api/src/models/session.model.ts
+++ api/src/models/session.model.ts
@@ -1,37 +1,24 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import config from "config";
+import { UserDocument } from "./user.model";
 
-export interface UserDocument extends mongoose.Document {
-  email: string;
-  name: string;
-  password: string;
+export interface SchemaDocument extends mongoose.Document {
+  user: UserDocument["_id"];
+  valid: boolean;
   createdAt: Date;
   updatedAt: Date;
-  comparePassword(candidatePassword: string): Promise<Boolean>;
 }
 
 const seesionSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
-    password: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    valid: { type: Boolean, default: true },
   },
   {
     timestamps: true,
   }
 );
-
-seesionSchema.pre("save", async function (next: (err?: Error) => void) {
-  let user = this as UserDocument;
-  if (!user.isModified("password")) {
-    return next();
-  }
-  const salt = await bcrypt.genSalt(config.get("saltWorkFactor"));
-  const hash = await bcrypt.hashSync(user.password, salt);
-  user.password = hash;
-  return next();
-});
 
 const SessionModel = mongoose.model("Session", seesionSchema);
 export default SessionModel;
