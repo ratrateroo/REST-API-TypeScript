9b84b5a5d4b6d544ed87b57ec11a3c066810892b
Mark_Leo <ratrateroo@gmail.com>
2022-10-31 21:27:33 +0800

    added user agent to schema type

--- api/src/models/session.model.ts
+++ api/src/models/session.model.ts
@@ -6,19 +6,21 @@ import { UserDocument } from "./user.model";
 export interface SchemaDocument extends mongoose.Document {
   user: UserDocument["_id"];
   valid: boolean;
+  userAgent: string;
   createdAt: Date;
   updatedAt: Date;
 }
 
-const seesionSchema = new mongoose.Schema(
+const sessionSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     valid: { type: Boolean, default: true },
+    userAgent: { type: String },
   },
   {
     timestamps: true,
   }
 );
 
-const SessionModel = mongoose.model("Session", seesionSchema);
+const SessionModel = mongoose.model("Session", sessionSchema);
 export default SessionModel;
