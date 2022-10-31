bc8158f8b0254a0f56205f21127a1892744784e8
Mark_Leo <ratrateroo@gmail.com>
2022-10-31 22:53:34 +0800

    added session controller with validate and session

---
+++ api/src/controller/session.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { createSession } from "../service/session.service";
+import { validatePassword } from "../service/user.service";
+export async function createUserSessionHandler(req: Request, res: Response) {
+  // validate the user's password
+  const user = await validatePassword(req.body);
+  if (!user) {
+    return res.status(401).send("Invalid email or password.");
+  }
+  // create a session
+  const session = createSession(user._id, req.get("user-agent") || "");
+  //TODO: create an access token
+  //TODO: create a refresh token
+  //TODO: return access & refresh token
+}
--- api/src/service/user.service.ts
+++ api/src/service/user.service.ts
@@ -1,4 +1,5 @@
 import { DocumentDefinition } from "mongoose";
+import { omit } from "lodash";
 import UserModel, { UserDocument } from "../models/user.model";
 import logger from "../utils/logger";
 
@@ -8,11 +9,34 @@ export async function createUser(
   >
 ) {
   try {
-    return await UserModel.create(input);
+    const user = UserModel.create(input);
+    return omit((await user).toJSON(), "password");
   } catch (error: unknown) {
     if (error instanceof Error) {
       logger.error(error);
       throw new Error(error.message);
     }
   }
+}
+
+export async function validatePassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const user = await UserModel.findOne({ email });
+
+  if (!user) {
+    return false;
+  }
+
+  const isValid = await user.comparePassword(password);
+
+  if (!isValid) {
+    return false;
+  }
+
+  return omit((await user).toJSON(), "password");
 }
