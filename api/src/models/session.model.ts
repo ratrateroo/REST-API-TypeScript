4c793413b66347d903ecf4ef2660a8ed968c83f0
Mark_Leo <ratrateroo@gmail.com>
2022-11-10 23:47:01 +0800

    fixed session model bug

--- api/src/models/session.model.ts
+++ api/src/models/session.model.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { UserDocument } from "./user.model";
 
-export interface SessionDocument extends mongoose.Document {
+export interface SchemaDocument extends mongoose.Document {
   user: UserDocument["_id"];
   valid: boolean;
   userAgent: string;
@@ -20,5 +20,5 @@ const sessionSchema = new mongoose.Schema(
   }
 );
 
-const SessionModel = mongoose.model<SessionDocument>("Session", sessionSchema);
+const SessionModel = mongoose.model<SchemaDocument>("Session", sessionSchema);
 export default SessionModel;
