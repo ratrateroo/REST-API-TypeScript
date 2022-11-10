1d076a812aaf0e481a9705dfcd348b0fd28ed65c
Mark_Leo <ratrateroo@gmail.com>
2022-11-11 00:27:31 +0800

    added find session service

--- api/src/service/session.service.ts
+++ api/src/service/session.service.ts
@@ -1,4 +1,5 @@
-import SessionModel from "../models/session.model";
+import { FilterQuery } from "mongoose";
+import SessionModel, { SchemaDocument } from "../models/session.model";
 
 export async function createSession(userId: string, userAgent: string) {
   const session = await SessionModel.create({
@@ -6,4 +7,8 @@ export async function createSession(userId: string, userAgent: string) {
     userAgent: userAgent,
   });
   return session.toJSON();
+}
+
+export async function findSessions(query: FilterQuery<SchemaDocument>) {
+  return SessionModel.find(query).lean();
 }
