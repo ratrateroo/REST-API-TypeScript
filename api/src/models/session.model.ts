4424602ee0ddcd5aa4573436e577634993b16971
Mark_Leo <ratrateroo@gmail.com>
2022-11-17 23:10:27 +0800

    fixed session model

--- api/src/models/session.model.ts
+++ api/src/models/session.model.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { UserDocument } from "./user.model";
 
-export interface SchemaDocument extends mongoose.Document {
+export interface SessionDocument extends mongoose.Document {
   user: UserDocument["_id"];
   valid: boolean;
   userAgent: string;
@@ -20,5 +20,5 @@ const sessionSchema = new mongoose.Schema(
   }
 );
 
-const SessionModel = mongoose.model<SchemaDocument>("Session", sessionSchema);
+const SessionModel = mongoose.model<SessionDocument>("Session", sessionSchema);
 export default SessionModel;
