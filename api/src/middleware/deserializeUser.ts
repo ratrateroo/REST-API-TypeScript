7c6d4fb00499925674b0690bc3a374db628411b5
Mark_Leo <ratrateroo@gmail.com>
2022-11-21 23:40:01 +0800

    updated deserialize user middleware

--- api/src/middleware/deserializeUser.ts
+++ api/src/middleware/deserializeUser.ts
@@ -1,11 +1,18 @@
 import { get } from "lodash";
 import { Request, Response, NextFunction } from "express";
 import { verifyJwt } from "../utils/jwt.utils";
-const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
+import { reIssueAccessToken } from "../service/session.service";
+const deserializeUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const accessToken = get(req, "headers.authorization", "").replace(
     /^Bearer\s/,
     ""
   );
+
+  const refreshToken = get(req, "headers.x-refresh");
 
   if (!accessToken) {
     return next();
@@ -14,6 +21,19 @@ const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
 
   if (decoded) {
     res.locals.user = decoded;
+
+    return next();
+  }
+
+  if (expired && refreshToken) {
+    const newAccessToken = await reIssueAccessToken({
+      refreshToken: refreshToken,
+    });
+    if (newAccessToken) {
+      res.setHeader("x-access-token", newAccessToken);
+    }
+    const result = verifyJwt(newAccessToken);
+    res.locals.user = result.decoded;
     return next();
   }
 
