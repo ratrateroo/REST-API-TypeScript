2fec5f6123c9e5cc450652b7e6f9a3d0ff217656
Mark_Leo <ratrateroo@gmail.com>
2022-11-28 22:27:43 +0800

    added update product to controller

--- api/src/controller/product.controller.ts
+++ api/src/controller/product.controller.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from "express";
+import { find } from "lodash";
 import {
   CreateProductInput,
   UpdateProductInput,
 } from "../schema/product.schema";
-import { createProduct } from "../service/product.service";
+import {
+  createProduct,
+  findAndUpdateProduct,
+  findProduct,
+} from "../service/product.service";
 
 export async function createProductHandler(
   req: Request<{}, {}, CreateProductInput["body"]>,
@@ -17,11 +22,41 @@ export async function createProductHandler(
 export async function updateProductHandler(
   req: Request<UpdateProductInput["params"]>,
   res: Response
-) {}
+) {
+  const userId = res.locals.user._id;
+  const productId = req.params.productId;
+  const update = req.body;
+  const product = await findProduct({ productId });
+  if (!product) {
+    return res.sendStatus(404);
+  }
+  if (product.user !== userId) {
+    return res.sendStatus(403);
+  }
+  const updatedProduct = await findAndUpdateProduct({ productId }, update, {
+    new: true,
+  });
+  return res.send(updatedProduct);
+}
 export async function getProductHandler(
   req: Request<UpdateProductInput["params"]>,
   res: Response
-) {}
+) {
+  const userId = res.locals.user._id;
+  const productId = req.params.productId;
+  const update = req.body;
+  const product = await findProduct({ productId });
+  if (!product) {
+    return res.sendStatus(404);
+  }
+  if (product.user !== userId) {
+    return res.sendStatus(403);
+  }
+  const updatedProduct = await findAndUpdateProduct({ productId }, update, {
+    new: true,
+  });
+  return res.send(updatedProduct);
+}
 export async function deleteProductHandler(
   req: Request<UpdateProductInput["params"]>,
   res: Response
