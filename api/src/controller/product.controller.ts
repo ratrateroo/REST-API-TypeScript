7fa4cae211988bdbbb276ae0a526ca2682ef7cce
Mark_Leo <ratrateroo@gmail.com>
2022-11-30 23:23:41 +0800

    added get product handler controller

--- api/src/controller/product.controller.ts
+++ api/src/controller/product.controller.ts
@@ -43,20 +43,14 @@ export async function getProductHandler(
   req: Request<UpdateProductInput["params"]>,
   res: Response
 ) {
-  const userId = res.locals.user._id;
   const productId = req.params.productId;
-  const update = req.body;
+
   const product = await findProduct({ productId });
   if (!product) {
     return res.sendStatus(404);
   }
-  if (product.user !== userId) {
-    return res.sendStatus(403);
-  }
-  const updatedProduct = await findAndUpdateProduct({ productId }, update, {
-    new: true,
-  });
-  return res.send(updatedProduct);
+
+  return res.send(product);
 }
 export async function deleteProductHandler(
   req: Request<UpdateProductInput["params"]>,
