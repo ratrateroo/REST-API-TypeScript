408dd69ca4a20f2f5d8fcc95f9ab9f76ded80b1b
Mark_Leo <ratrateroo@gmail.com>
2022-12-01 21:55:41 +0800

    added post product route

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,4 +1,5 @@
 import { Express, Request, Response } from "express";
+import { createProductHandler } from "./controller/product.controller";
 import {
   createUserSessionHandler,
   deleteSessionHandler,
@@ -7,6 +8,7 @@ import {
 import { createUserHandler } from "./controller/user.controller";
 import requireUser from "./middleware/requireUser";
 import validateResource from "./middleware/validateResource";
+import { createProductSchema } from "./schema/product.schema";
 import { createSessionSchema } from "./schema/session.schema";
 import { createUserSchema } from "./schema/user.schema";
 
@@ -26,6 +28,12 @@ function routes(app: Express) {
   app.get("/api/sessions", requireUser, getUserSessionsHandler);
 
   app.get("/api/sessions", requireUser, deleteSessionHandler);
+
+  app.post("/app/products", [
+    requireUser,
+    validateResource(createProductSchema),
+    createProductHandler,
+  ]);
 }
 
 export default routes;
