ccfa706df0245e15093c9b81b0a9797eb44f1469
Mark_Leo <ratrateroo@gmail.com>
2022-12-12 23:31:10 +0800

    updated sample code for testing

--- testing-state/src/ParentLevel.js
+++ testing-state/src/ParentLevel.js
@@ -8,21 +8,27 @@ export default function ParentLevel() {
 
   const setCountHandler = () => {
     // setCount((previewsValue) => previewsValue.value + 1);
+
     setCount((previewsValue) => {
-      return { ...previewsValue, value: previewsValue + 1 };
+      console.log("previewsValue");
+      console.log(previewsValue);
+      return { ...previewsValue, value: previewsValue.value + 1 };
     });
   };
 
   useEffect(() => {
-    // setCount2(count);
-    console.log(count.value);
+    console.log("useEffect");
+    console.log(count2);
+    setCount2({ ...count });
+    // console.log(count.value);
   }, [count]);
-
+  console.log("Log");
+  console.log(count2);
   return (
     <>
       <h1>ParentLevel</h1>
       <h2>Count: {count.value} </h2>
-      {/* <h2>Count2: {count2.value} </h2> */}
+      {/* <h2>Count2: {count2} </h2> */}
 
       <Sibling1 count={count} setCountHandler={setCountHandler} />
       <Sibling2 count={count} />
