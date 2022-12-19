ee40203fe599e6dc7d28c938dbe0e8d39abc34bf
Mark_Leo <ratrateroo@gmail.com>
2022-12-19 21:10:30 +0800

    updated routes

--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -6,8 +6,6 @@ const router = express.Router();
 
 router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/user/:uid", placesControllers.getPlacesByUserId);
-
 router.post("/", placesControllers.createPlace);
 
 router.patch("/:pid", placesControllers.updatePlace);
