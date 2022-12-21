fab341f2b69d5c2d4cbc7b956a59503df5f277ba
Mark_Leo <ratrateroo@gmail.com>
2022-12-21 19:35:59 +0800

    changed for survey

--- server/controllers/survey-controllers.js
+++ server/controllers/survey-controllers.js
@@ -16,24 +16,24 @@ let DUMMY_PLACES = [
   },
 ];
 
-const getPlaceById = (req, res, next) => {
+const getSurveyById = (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
 
-  const place = DUMMY_PLACES.find((p) => {
+  const survey = DUMMY_PLACES.find((p) => {
     return p.id === placeId;
   });
 
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
+  if (!survey) {
+    throw new HttpError("Could not find a survey for the provided id.", 404);
   }
 
-  res.json({ place }); // => { place } => { place: place }
+  res.json({ survey }); // => { survey } => { survey: survey }
 };
 
-// function getPlaceById() { ... }
-// const getPlaceById = function() { ... }
+// function getSurveyById() { ... }
+// const getSurveyById = function() { ... }
 
-const getPlacesByUserId = (req, res, next) => {
+const getSurveysByUserId = (req, res, next) => {
   const userId = req.params.uid;
 
   const places = DUMMY_PLACES.filter((p) => {
@@ -49,7 +49,7 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = async (req, res, next) => {
+const createSurvey = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(
@@ -67,7 +67,7 @@ const createPlace = async (req, res, next) => {
   }
 
   // const title = req.body.title;
-  const createdPlace = {
+  const createdSurvey = {
     id: uuidv4(),
     title,
     description,
@@ -76,12 +76,12 @@ const createPlace = async (req, res, next) => {
     creator,
   };
 
-  DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)
+  DUMMY_PLACES.push(createdSurvey); //unshift(createdSurvey)
 
-  res.status(201).json({ place: createdPlace });
+  res.status(201).json({ survey: createdSurvey });
 };
 
-const updatePlace = (req, res, next) => {
+const updateSurvey = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError("Invalid inputs passed, please check your data.", 422);
@@ -90,27 +90,27 @@ const updatePlace = (req, res, next) => {
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+  const updatedSurvey = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
   const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
-  updatedPlace.title = title;
-  updatedPlace.description = description;
+  updatedSurvey.title = title;
+  updatedSurvey.description = description;
 
-  DUMMY_PLACES[placeIndex] = updatedPlace;
+  DUMMY_PLACES[placeIndex] = updatedSurvey;
 
-  res.status(200).json({ place: updatedPlace });
+  res.status(200).json({ survey: updatedSurvey });
 };
 
-const deletePlace = (req, res, next) => {
+const deleteSurvey = (req, res, next) => {
   const placeId = req.params.pid;
   if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
-    throw new HttpError("Could not find a place for that id.", 404);
+    throw new HttpError("Could not find a survey for that id.", 404);
   }
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
-  res.status(200).json({ message: "Deleted place." });
+  res.status(200).json({ message: "Deleted survey." });
 };
 
-exports.getPlaceById = getPlaceById;
-exports.getPlacesByUserId = getPlacesByUserId;
-exports.createPlace = createPlace;
-exports.updatePlace = updatePlace;
-exports.deletePlace = deletePlace;
+exports.getSurveyById = getSurveyById;
+exports.getSurveysByUserId = getSurveysByUserId;
+exports.createSurvey = createSurvey;
+exports.updateSurvey = updateSurvey;
+exports.deleteSurvey = deleteSurvey;
