280faff33ce01ab2ae5f87bba288c08a82384c47
Mark_Leo <ratrateroo@gmail.com>
2023-01-26 01:15:47 +0800

    tried some codes

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Page1 from "./components/Page1";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(100);
+
+  const countHandler = (value: number) => {
+    setCount((previousValue) => previousValue + value);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Count: {count}</h1>
+      <Page1 count={count} countHandler={countHandler} />
+      <button
+        onClick={() => {
+          countHandler(1);
+        }}
+      >
+        Click
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-react-app/src/components/Page1.tsx
+++ my-react-app/src/components/Page1.tsx
@@ -1,14 +1,25 @@
 import { useState, useEffect } from "react";
 
-function Page1(props: {}) {
-  const [value, setValue] = useState();
+function Page1(props: { count: number; countHandler: (arg0: number) => void }) {
+  const [value, setValue] = useState(props.count);
+  const [value2, setValue2] = useState(201);
 
   useEffect(() => {
+    addUp(1);
     console.log("with dependency array");
-  }, []);
+    console.log(value2);
+    setValue2((previousValue) => previousValue + 1);
+    addUp(1);
+  }, [value]);
+
+  const addUp = (value: number) => {
+    props.countHandler(value);
+  };
   return (
     <div>
       <h1>Page 1</h1>
+      <h1>{value}</h1>
+      <h1>{value2}</h1>
     </div>
   );
 }
